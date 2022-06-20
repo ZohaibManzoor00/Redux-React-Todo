cdedf1ffea1d61bc5fd06e120e9026defcf2f66e
Matthew <tanmatt804@gmail.com>
2022-06-20 17:58:36 -0400

    added MVC

---
+++ todoBackEnd/models/todoModel.js
@@ -0,0 +1,8 @@
+const db = require('../db/db');
+
+class ToDo {
+    static getToDosFromDB = () => db.select().from('reduxtodo').orderBy('id');
+    static postToDoFromDB = (description) => db('reduxtodo').insert({description, completed: false}).returning('*');
+    static updateToDoFromDB = (id) => db('reduxtodo').update({completed: true}).where({id}).returning('*');
+    static deleteToDoFromDB = (id) => db.select().from('reduxtodo').where({id}).del();
+}
